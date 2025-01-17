970eb5c749f9cae9d98bda8cefd6d025c96f91a2
Z7L7 <asiyah.adetunji.workplace@gmail.com>
2025-01-16 21:57:57 -0700

    News

---
+++ pages/news.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import requests
+from datetime import datetime
+import time
+import streamlit as st
+import numpy as np
+import pickle
+def app():
+    st.title("GDELT")
+    st.write("Welcome to the Humanitarian Aid Website!")
