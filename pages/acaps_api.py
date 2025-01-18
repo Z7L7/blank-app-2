7f77288e51249dcb49cc02b5a9f0573f7bbf3bec
Z7L7 <asiyah.adetunji.workplace@gmail.com>
2025-01-17 17:32:23 -0700

    Test Homepage Addition Two

--- pages/acaps_api.py
+++ pages/acaps_api.py
@@ -15,59 +15,59 @@ def app():
     st.title("Acaps")
     st.write("Welcome to the Humanitarian Aid Website!")
 
-country = st.text_input("", "syria")
-st.write("The current country shown is:", country)
+    country = st.text_input("", "syria")
+    st.write("The current country shown is:", country)
 
-# Post credentials to get an authentication token
-credentials = {
-    "username": "asiyah.adetunji.workplace@gmail.com", # Replace with your email address
-    "password": "sPAcA317&2" # Replace with your password
-}
-auth_token_response = requests.post("https://api.acaps.org/api/v1/token-auth/", credentials)
-auth_token = auth_token_response.json()['token']
+    # Post credentials to get an authentication token
+    credentials = {
+        "username": "asiyah.adetunji.workplace@gmail.com", # Replace with your email address
+        "password": "sPAcA317&2" # Replace with your password
+    }
+    auth_token_response = requests.post("https://api.acaps.org/api/v1/token-auth/", credentials)
+    auth_token = auth_token_response.json()['token']
 
-# Pull data from ACAPS API, loop through the pages, and append to a pandas DataFrame
-df = pd.DataFrame()
-request_url = ("https://api.acaps.org/api/v1/risk-list/?country=%s" %country) # Replace with the URL of the dataset you want to access
-last_request_time = datetime.now()
-while True:
-
-    # Wait to avoid throttling
-    while (datetime.now()-last_request_time).total_seconds() < 1:
-        time.sleep(0.1)
-
-    # Make the request
-    response = requests.get(request_url, headers={"Authorization": "Token %s" % auth_token})
+    # Pull data from ACAPS API, loop through the pages, and append to a pandas DataFrame
+    df = pd.DataFrame()
+    request_url = ("https://api.acaps.org/api/v1/risk-list/?country=%s" %country) # Replace with the URL of the dataset you want to access
     last_request_time = datetime.now()
-    response = response.json()
-    print(response)
+    while True:
 
-    # Append to a pandas DataFrame
-    df = df._append(pd.DataFrame(response["results"]))
+        # Wait to avoid throttling
+        while (datetime.now()-last_request_time).total_seconds() < 1:
+            time.sleep(0.1)
 
-    # Loop to the next page; if we are on the last page, break the loop
-    if ("next" in response.keys()) and (response["next"] != None):
-        request_url = response["next"]
-    else: break
+        # Make the request
+        response = requests.get(request_url, headers={"Authorization": "Token %s" % auth_token})
+        last_request_time = datetime.now()
+        response = response.json()
+        print(response)
+
+        # Append to a pandas DataFrame
+        df = df._append(pd.DataFrame(response["results"]))
+
+        # Loop to the next page; if we are on the last page, break the loop
+        if ("next" in response.keys()) and (response["next"] != None):
+            request_url = response["next"]
+        else: break
 
 
-# - - - Front End - - -
-st.header("Data")
-st.dataframe(df) 
-url = "https://www.acaps.org/en/"
-st.write("This is the Risk List dataset from [ACAPS.org](%s) a nonprofit, nongovernmental website." % url)
+    # - - - Front End - - -
+    st.header("Data")
+    st.dataframe(df) 
+    url = "https://www.acaps.org/en/"
+    st.write("This is the Risk List dataset from [ACAPS.org](%s) a nonprofit, nongovernmental website." % url)
 
 
 
-container = st.container(border=True)
-df2 = df[df['rationale'] != '[-]']
-container.header("Rationale")
-container.write(df2.to_string(columns=['rationale'], header=True, index=True))
-# tab1, tab2 = st.tabs(["syria", "yemen"])
+    container = st.container(border=True)
+    df2 = df[df['rationale'] != '[-]']
+    container.header("Rationale")
+    container.write(df2.to_string(columns=['rationale'], header=True, index=True))
+    # tab1, tab2 = st.tabs(["syria", "yemen"])
 
-# country = st.text_input("", "")
-# st.write("The current country shown is:", country)
+    # country = st.text_input("", "")
+    # st.write("The current country shown is:", country)
 
-# tab1.subheader("Syria")
-# df2 = df[df['rationale'] != '[-]']
-# st.write(df2.to_string(columns=['rationale'], header=True, index=True))
+    # tab1.subheader("Syria")
+    # df2 = df[df['rationale'] != '[-]']
+    # st.write(df2.to_string(columns=['rationale'], header=True, index=True))
--- pages/homepage.py
+++ pages/homepage.py
@@ -4,20 +4,20 @@ def app():
     st.title("Homepage")
     st.write("Welcome to the Humanitarian Aid Website!")
 
-# - - - Columns with images - - -
-col1, col2 = st.columns(2)
+    # - - - Columns with images - - -
+    col1, col2 = st.columns(2)
 
-with col1:
-    st.image("https://i.pinimg.com/474x/91/58/87/9158873487dc0d6fdad74b0dd1aed074.jpg", caption="")
+    with col1:
+        st.image("https://i.pinimg.com/474x/91/58/87/9158873487dc0d6fdad74b0dd1aed074.jpg", caption="")
 
-with col2:
-    st.header("What is Humanitarian Aid?")
-    st.write("Humanitarian aid is an important subject to many across the world. It is defined as the assitance provided to those in need using both logistic and material assistance. There are many humanitarian aid organizations such as UNICEF and Doctors Without Boarders")
+    with col2:
+        st.header("What is Humanitarian Aid?")
+        st.write("Humanitarian aid is an important subject to many across the world. It is defined as the assitance provided to those in need using both logistic and material assistance. There are many humanitarian aid organizations such as UNICEF and Doctors Without Boarders")
 
-# - - - Text - - -
-coli3, coli4 = st.columns(2)
-with coli3:
-    st.image("https://i.pinimg.com/474x/b8/19/79/b819799f9ab52866a70218fd23499519.jpg", caption="")
+    # - - - Text - - -
+    coli3, coli4 = st.columns(2)
+    with coli3:
+        st.image("https://i.pinimg.com/474x/b8/19/79/b819799f9ab52866a70218fd23499519.jpg", caption="")
 
-with coli4:
-    st.image("https://i.pinimg.com/474x/93/1d/62/931d62501c149f08ec1643592081754e.jpg", caption="")
+    with coli4:
+        st.image("https://i.pinimg.com/474x/93/1d/62/931d62501c149f08ec1643592081754e.jpg", caption="")
