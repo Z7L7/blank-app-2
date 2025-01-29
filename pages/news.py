c751ac4be1d42aabdcab92151c41629105da90cb
Z7L7 <asiyah.adetunji.workplace@gmail.com>
2025-01-28 20:28:32 -0700

    get rid of News part

--- pages/news.py
+++ pages/news.py
@@ -12,64 +12,91 @@ def app():
     st.title("News")
     st.write("Welcome to the Humanitarian Aid Website!")
 
-    # Get the list of countries and keywords
-    countries = sorted(country_code.keys())
-    keywords = sorted(crisis_cameo_codes.keys())
+    #  # Get the list of countries and keywords
+    # countries = sorted(country_code.keys())
+    # keywords = sorted(crisis_cameo_codes.keys())
+
+    # # Dropdown for countries
+    # # country = st.selectbox("Select a Country", countries)
+
+    # # Dropdown for keywords
+    # # keyword = st.selectbox("Select a Keyword", keywords)
+    # st.title("Acaps")
+    # st.write("Welcome to the Humanitarian Aid Website!")
+
+    # # country = st.text_input("", "afghanistan")
+    # # st.write("The current country shown is:", country)
+    # # et the list of countries and keywords
+    # countries = sorted(country_code.keys())
+    # # keywords = sorted(crisis_cameo_codes.keys())
 
     # # Dropdown for countries
     # country = st.selectbox("Select a Country", countries)
 
     # # Dropdown for keywords
     # keyword = st.selectbox("Select a Keyword", keywords)
-    st.title("Acaps")
-    st.write("Welcome to the Humanitarian Aid Website!")
 
-    # country = st.text_input("", "afghanistan")
-    # st.write("The current country shown is:", country)
-    # Get the list of countries and keywords
-    countries = sorted(country_code.keys())
-    #keywords = sorted(crisis_cameo_codes.keys())
+    # # Post credentials to get an authentication token
+    # credentials = {
+    #     "username": "asiyah.adetunji.workplace@gmail.com", # Replace with your email address
+    #     "password": "sPAcA317&2" # Replace with your password
+    # }
+    # auth_token_response = requests.post("https://api.acaps.org/api/v1/token-auth/", credentials)
+    # auth_token = auth_token_response.json()['token']
 
-    # Dropdown for countries
-    # country = st.selectbox("Select a Country", countries)
+    # # Pull data from ACAPS API, loop through the pages, and append to a pandas DataFrame
+    # df = pd.DataFrame()
+    # request_url = ("https://api.acaps.org/api/v1/yemen-monitoring/") # Replace with the URL of the dataset you want to access
+    # last_request_time = datetime.now()
+    # while True:
 
-    # Dropdown for keywords
-    #keyword = st.selectbox("Select a Keyword", keywords)
+        # # Wait to avoid throttling
+        # while (datetime.now()-last_request_time).total_seconds() < 1:
+        #     time.sleep(0.1)
 
-    # Post credentials to get an authentication token
-    credentials = {
-        "username": "asiyah.adetunji.workplace@gmail.com", # Replace with your email address
-        "password": "sPAcA317&2" # Replace with your password
-    }
-    auth_token_response = requests.post("https://api.acaps.org/api/v1/token-auth/", credentials)
-    auth_token = auth_token_response.json()['token']
+        # # Make the request
+        # response = requests.get(request_url, headers={"Authorization": "Token %s" % auth_token})
+        # last_request_time = datetime.now()
+        # response = response.json()
+        # print(response)
 
-    # Pull data from ACAPS API, loop through the pages, and append to a pandas DataFrame
-    df = pd.DataFrame()
-    request_url = ("https://api.acaps.org/api/v1/yemen-monitoring/") # Replace with the URL of the dataset you want to access
-    last_request_time = datetime.now()
-    while True:
-
-        # Wait to avoid throttling
-        while (datetime.now()-last_request_time).total_seconds() < 1:
-            time.sleep(0.1)
-
-        # Make the request
-        response = requests.get(request_url, headers={"Authorization": "Token %s" % auth_token})
-        last_request_time = datetime.now()
-        response = response.json()
-        print(response)
-
-        # Append to a pandas DataFrame
-        df = df._append(pd.DataFrame(response["results"]))
+        # # Append to a pandas DataFrame
+        # df = df._append(pd.DataFrame(response["results"]))
 
         # Loop to the next page; if we are on the last page, break the loop
-        if ("next" in response.keys()) and (response["next"] != None):
-            request_url = response["next"]
-        else: break
+        # if ("next" in response.keys()) and (response["next"] != None):
+        #     request_url = response["next"]
+        # else: break
 
 
-    # - - - Front End - - -
-    st.header("Data")
-    st.dataframe(df) 
+        # st.dataframe(df) 
+    # with st.expander('Data'):
+    #     st.write("Raw data")
+    #     st.dataframe(df)
+
+    #     st.write("X Variables")
+    #     X = df.drop(columns="risk_level", axis=1)
+    #     st.dataframe(X)
+
+    #     st.write("Y Variables")
+    #     y = df.risk_level
+    #     st.dataframe(y)
+
+    #     rows = {
+
+    #     }
+
+    # # Graphs
+    # with st.expander('Data Visualiser'):
+    #     st.bar_chart(data=df, x="risk_level", y="risk_type", color="impact")
+
+    # # Data preferation
+    # with st.sidebar:
+    #     st.header('Input Features')
+    #     count = st.selectbox('country')
+
+
+    # # - - - Front End - - -
+    # st.header("Data")
+    # st.dataframe(df) 
 
