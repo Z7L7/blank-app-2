8eeff511f45d26d724a8fe06354ad8831453527d
Z7L7 <asiyah.adetunji.workplace@gmail.com>
2025-03-26 17:41:10 -0600

    Fix Session State

--- pages/analysis.py
+++ pages/analysis.py
@@ -6,7 +6,7 @@ from modules.charts import time_series_chart, bar_chart, word_cloud_image
 from datetime import datetime
 import requests
 import time
-import scipy
+# import scipy
 
 def app():
     st.title("Full Analysis Page")
@@ -63,27 +63,27 @@ def app():
             request_url = response["next"]
         else: break
 
-        import streamlit as st
+        # import streamlit as st
 
-        import numpy as np
-        import plotly.figure_factory as ff
+        # import numpy as np
+        # import plotly.figure_factory as ff
 
-        # Add histogram data
-        x1 = np.random.randn(200) - 2
-        x2 = np.random.randn(200)
-        x3 = np.random.randn(200) + 2
+        # # Add histogram data
+        # x1 = np.random.randn(200) - 2
+        # x2 = np.random.randn(200)
+        # x3 = np.random.randn(200) + 2
 
-        # Group data together
-        hist_data = [x1, x2, x3]
+        # # Group data together
+        # hist_data = [x1, x2, x3]
 
-        group_labels = ['Group 1', 'Group 2', 'Group 3']
+        # group_labels = ['Group 1', 'Group 2', 'Group 3']
 
-        # Create distplot with custom bin_size
-        fig = ff.create_distplot(
-                hist_data, group_labels, bin_size=[.1, .25, .5])
+        # # Create distplot with custom bin_size
+        # fig = ff.create_distplot(
+        #         hist_data, group_labels, bin_size=[.1, .25, .5])
 
-        # Plot!
-        st.plotly_chart(fig)
+        # # Plot!
+        # st.plotly_chart(fig)
 
     
 
